ce08cb4a356b45fd8c512330f27963e113d238f5
Aakash Shah <ashah@lowes.com>
2020-01-01 16:18:16 +0530

    setting up a basic server

---
+++ src/server/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+    const config = {
+        port: 3000,
+        host: 'localhost'
+    };
+    const server = Hapi.server(config);
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
